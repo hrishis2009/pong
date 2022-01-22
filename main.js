ab343a20816751e1fe4b7b60884a2689be6f2568
shanmuga1980 <84486730+shanmuga1980@users.noreply.github.com>
2022-01-22 13:25:28 -0500

    Add cookies and add online/offline detector

---
+++ main.js
@@ -0,0 +1,21 @@
+function cookies() {
+	navigator.cookieEnabled;
+	return this;
+}
+
+let const cookies = cookies();
+
+if (cookies !== true) {
+	window.alert("Cookies are not enales for this website. Please enable cookies so you can enjoy a better experience");
+}
+
+function online() {
+	navigator.onLine;
+	return this;
+}
+
+var offline = setInterval(online, 2500);
+
+function setcookies() {
+	document.cookie="online=" + offline;
+}
