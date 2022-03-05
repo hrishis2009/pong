e84de8334cb18be8655b0f92aa16489387751dc8
shanmuga1980 <84486730+shanmuga1980@users.noreply.github.com>
2022-03-05 11:12:00 -0500

    Update play.js

--- pong/play.js
+++ pong/play.js
@@ -1,1 +1,8 @@
-
+function play() {
+  let playScreen = new Ingame();
+  document.getElementById("homescreen_panel").remove();
+  var playing = true;
+  var score = 0;
+  let playerObj = document.getElementById("paddle_movement_2");
+  // movement logic
+}
