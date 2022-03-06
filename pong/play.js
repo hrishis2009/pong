ceac050953479e2bab40c55431a61db6ab740ec1
shanmuga1980 <84486730+shanmuga1980@users.noreply.github.com>
2022-03-05 21:57:59 -0500

    Update play.js

--- pong/play.js
+++ pong/play.js
@@ -1,9 +1,11 @@
-function play() {
+var playing;
+if (playing == true) {
   let playScreen = new Ingame();
   document.getElementById("homescreen_panel").remove();
-  var playing = true;
   var score = 0;
   let playerObj = document.getElementById("paddle_movement_2");
+  let botPlayer = document.getElementById("paddle_movement_1");
+  let ball = document.getElementById("ball_movement");
   document.onkeydown = function(e) {
     if (e.keyCode == 38) {
       // up arrow
@@ -13,4 +15,19 @@ function play() {
       // down arrow
       playerObj.style.top = "-10px"
     }
+    
+    function paddleContactTest(div1, div2){
+      let ox = Math.abs(div1.x - div2.x) < (div1.x < div2.x ? div2.width : div1.width);
+      let oy = Math.abs(div1.y - div2.y) < (div1.y < div2.y ? div2.height : div1.height);
+      return ox && oy;
+    }
+    
+    var playerPaddleContact = paddleContactTest(playerObj, ball);
+    
+    function bounceBall() {
+      //bounce ball physics
+    }
+    
+    // switch case block to bounce if ball hits walls or hits an object
+    
 }
